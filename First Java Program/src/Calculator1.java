27b5823bd9178c07868051727966793d0bbaffb5
Amita Patel <amita70007@gmail.com>
2021-03-13 20:20:24 +0000

    my first java project

---
+++ First Java Program/src/Calculator1.java
@@ -0,0 +1,24 @@
+public class Calculator1 {
+
+
+        static int a = 10;
+        static int b = 5;
+        static int total;
+        public static void main (String[] args){
+
+            total = a+b;
+            System.out.println("total of a+b="+total);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+
+
+
+
+
+        }
+
+
+
+
+}
---
+++ First Java Program/src/PersonalDetails.java
@@ -0,0 +1,24 @@
+public class PersonalDetails {
+
+    public static void main(String[] args) {
+        String name = "Arya";
+        String surname = "Patel";
+        String address = "23 Malvern Road, CR9 9JJ";
+        String mobile = "079123456789";
+        String age = "30";
+        String SecurityCode = "35.60";
+        boolean EligibleForVote = true;
+        String grade = "b";
+
+        System.out.println(name);
+        System.out.println("SURName:"+surname);
+        System.out.println("Address:"+address);
+        System.out.println("Mobile NO:"+mobile);
+        System.out.println("AGE:"+age);
+        System.out.println("Security Code:"+SecurityCode);
+        System.out.println("Is Eligible for Vote: "+EligibleForVote);
+        System.out.println("Grade:"+grade);
+    }
+
+
+}
